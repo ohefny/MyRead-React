15fd118500b0dd26b33c2736073695db24e6b55c
Omar El Hefny <o.hefny@gmail.com>
2020-02-22 11:07:06 +0200

    refactor: make Shelf accept id and title

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -5,7 +5,10 @@ import BooksList from "./BooksList";
 class Shelf extends Component {
     static propTypes = {
         books: PropTypes.array.isRequired,
-        title: PropTypes.string,
+        metadata: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired
+        }),
         onBookShelfChanged: PropTypes.func.isRequired
     };
     state = {};
@@ -14,9 +17,10 @@ class Shelf extends Component {
         return (
             <div className="bookshelf">
                 <div className="bookshelf-title">
-                    <h1>{this.props.name}</h1>
+                    <h1>{this.props.metadata.title}</h1>
                 </div>
-                <BooksList className="bookshelf-books" books={this.props.books} onBookShelfChanged={this.props.onBookShelfChanged}/>
+                <BooksList className="bookshelf-books" books={this.props.books}
+                           onBookShelfChanged={this.props.onBookShelfChanged}/>
             </div>
         );
     }
