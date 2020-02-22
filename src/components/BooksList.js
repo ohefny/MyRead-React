640454735edcc792fbd8d2005c9ebc277f34279d
Omar El Hefny <o.hefny@gmail.com>
2020-02-22 05:15:29 +0200

    refactor: divide the component to smaller components

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types'
+import {BOOK_SHELF_OPTION} from "../consts";
 
 class BooksList extends Component {
     static propTypes = {
@@ -7,13 +8,55 @@ class BooksList extends Component {
         onBookShelfChanged: PropTypes.func.isRequired
     };
     state = {};
+
+    changeBookshelf(id) {
+        console.log(id)
+    }
+
     render() {
         return (
-            <ol>
-                {this.props.books.map(it=><li>{it.title}</li>)}
+            <ol className="books-grid">
+                {this.props.books.map(book =>
+                    <li>
+                        <BookItem book={book} changeBookshelf={this.changeBookshelf}/>
+                    </li>
+                )}
             </ol>
         )
     }
 }
+
+function BookItem(props) {
+    const {book, changeBookshelf} = props;
+    return <div className="book">
+        <div className="book-top">
+            <BookCover cover={book.cover}/>
+            <BookshelfChanger onBookShelfChanged={changeBookshelf} currentShelf={book.currentShelf}/>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.author}</div>
+    </div>;
+}
+
+function BookshelfChanger(props) {
+    return (<div className="book-shelf-changer">
+        <select defaultValue={props.currentShelf} onChange={(event) => props.onBookShelfChanged(event.target.value)}>
+            <option value="move" disabled>Move to...</option>
+            <option value={BOOK_SHELF_OPTION.CURRENTLY_READING}>Currently Reading</option>
+            <option value={BOOK_SHELF_OPTION.WANT_TO_READ}>Want to Read</option>
+            <option value={BOOK_SHELF_OPTION.READ}>Read</option>
+            <option value={BOOK_SHELF_OPTION.NONE}>None</option>
+        </select>
+    </div>)
+}
+
+function BookCover(props) {
+    return (<div className="book-cover" style={{
+        width: 128,
+        height: 193,
+        backgroundImage: `url(${props.cover})`
+    }}/>)
+}
+
 
 export default BooksList
---
+++ src/consts.js
@@ -0,0 +1,6 @@
+export const BOOK_SHELF_OPTION = {
+    NONE: 0,
+    READ: 1,
+    CURRENTLY_READING: 2,
+    WANT_TO_READ: 3
+};
